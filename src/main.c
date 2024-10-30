798fda120f467f3f8e13cf7acebdd44e7b1df916
Dangerdrive <felippealencar@gmail.com>
2024-10-30 18:02:48 -0300

    - Commit on 2024.10.30\t18h02m47

--- src/main.c
+++ src/main.c
@@ -9,21 +9,29 @@ int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
     return (r << 24 | g << 16 | b << 8 | a);
 }
 
-void ft_randomize(void* param)
+void	ft_randomize(void *param)
 {
+	uint32_t	i;
+	uint32_t	y;
+	uint32_t	color;
+
 	(void)param;
-	for (uint32_t i = 0; i < image->width; ++i)
+	i = 0;
+	while (i < image->width)
 	{
-		for (uint32_t y = 0; y < image->height; ++y)
+		y = 0;
+		while (y < image->height)
 		{
-			uint32_t color = ft_pixel(
-				rand() % 0xFF, // R
-				rand() % 0xFF, // G
-				rand() % 0xFF, // B
-				rand() % 0xFF  // A
+			color = ft_pixel(
+				100 + (rand() % (256 - 100)), // R
+				0,                            // G
+				0,                            // B
+				100 + (rand() % (256 - 100))  // A
 			);
 			mlx_put_pixel(image, i, y, color);
+			y++;
 		}
+		i++;
 	}
 }
 
@@ -50,17 +58,34 @@ int32_t main(void)
 	mlx_t* mlx;
 
 	// Gotta error check this stuff
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "CUB3D", true)))
 	{
 		puts(mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-	if (!(image = mlx_new_image(mlx, 128, 128)))
+
+	mlx_texture_t *texture_example = mlx_load_png("./textures/texture_example.png");
+	if (!texture_example)
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return(EXIT_FAILURE);		
 	}
+	// if (!(image = mlx_new_image(mlx, 200, 128)))
+	// {
+	// 	mlx_close_window(mlx);
+	// 	puts(mlx_strerror(mlx_errno));
+	// 	return(EXIT_FAILURE);
+	// }
+	image = mlx_texture_to_image(mlx, texture_example);	
+	
+
+	// if (!(image = mlx_new_image(mlx, 200, 128)))
+	// {
+	// 	mlx_close_window(mlx);
+	// 	puts(mlx_strerror(mlx_errno));
+	// 	return(EXIT_FAILURE);
+	// }
 	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
 	{
 		mlx_close_window(mlx);
@@ -68,11 +93,12 @@ int32_t main(void)
 		return(EXIT_FAILURE);
 	}
 	
-	mlx_loop_hook(mlx, ft_randomize, mlx);
+	//mlx_loop_hook(mlx, ft_randomize, mlx);
 	mlx_loop_hook(mlx, ft_hook, mlx);
 
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
+
 
